745d086f9754ee24307f0dbeecd2320b82cf9d98
Parkayun <iamparkayun@gmail.com>
2015-02-09 16:36:28 +0900

    Add handlers for path and filter exceptions

--- flask_init/exceptions.py
+++ flask_init/exceptions.py
@@ -1,2 +1,18 @@
-class PathAlreadyExists(Exception):
+class ProjectPathAlreadyExists(Exception):
     """Base class for Project path already exists"""
+
+
+class StaticPathAlreadyExists(Exception):
+    """Base class for Static path already exists"""
+
+
+class TemplatePathAlreadyExists(Exception):
+    """Base class for Template path already exists"""
+
+
+class ModulePathAlreadyExists(Exception):
+    """Base class for Module path already exists"""
+
+
+class InvalidProjectName(Exception):
+    """Base Class for Invalid project name"""
--- flask_init/run.py
+++ flask_init/run.py
@@ -4,22 +4,50 @@ import os
 
 import six
 
-from .exceptions import PathAlreadyExists
+from .exceptions import *
 from .templates import *
 
 
-def create_assets(_proj_path):
-    os.mkdir(os.path.join(_proj_path, "templates"))
-    os.mkdir(os.path.join(_proj_path, "static"))
+def create_folder(_folder_path, _exception):
+    if os.path.exists(_folder_path):
+        raise _exception
+    os.mkdir(_folder_path)
 
 
-def create_module(_app_path, module):
-    os.mkdir(os.path.join(_app_path, module))
+def get_result_and_create_assets(_proj_path):
+    result = []
+
+    data = (
+        ("templates", TemplatePathAlreadyExists),
+        ("static", StaticPathAlreadyExists),
+    )
+
+    for folder, exception in data:
+        try:
+            create_folder(os.path.join(_proj_path, folder), exception)
+        except exception:
+            error = folder.title() + " folder create skipped (alreay exists)"
+            results.append(error)
+
+    return result
+
+
+def get_result_and_create_module(_app_path, module):
+    result = []
     module_path = os.path.join(_app_path, module)
+    try:
+        if os.path.exists(module_path):
+            raise ModulePathAlreadyExists
+        os.mkdir(os.path.join(_app_path, module))
+    except ModulePathAlreadyExists:
+        result.append("Module folder create skipped (already exists)")
+
     create_py(os.path.join(module_path, "__init__.py"), 
             module_init_template.substitute(module=module))
     create_py(os.path.join(module_path, "views.py"), blank_template)
     create_py(os.path.join(module_path, "models.py"), blank_template)
+
+    return result
 
 
 def create_py(py_path, content):
@@ -27,39 +55,57 @@ def create_py(py_path, content):
         py.write(content)
 
 
+def check_name(_name):
+    _filter = "!@#$%^&*()-+=[]{}|\"'."
+    if len(list(set(list(_name)).intersection(list(_filter)))) > 0:
+        return False
+    if _name[0].isdigit():
+        return False
+    return True
+
+
 def main():
     inputs = (
-        ('name', 'Input project name: '),
-        ('module', 'Input module name (default \'base\'): '),
+        ('name', 'Input project name (default is "flask_proj)": '),
+        ('module', 'Input module name (default is "base"): '),
     )
     values = {}
+    result = []
 
     for title, question in inputs:
         values[title] = six.moves.input(question)
 
-    values['module'] = values.get('module') or 'base'
-
-    proj_path = os.path.join(os.getcwd(), values['name'])
-
     try:
+        values['name'] = values.get('name') or "flask_proj"
+        if not check_name(values['name']):
+            raise InvalidProjectName
+        values['module'] = values.get('module') or "base"
+
+        proj_path = os.path.join(os.getcwd(), values['name'])
+
         if os.path.exists(proj_path):
-           raise PathAlreadyExists
+           raise ProjectPathAlreadyExists
 
         os.mkdir(proj_path)
         create_py(os.path.join(proj_path, "manager.py"), manager_template)
 
-        create_assets(proj_path)
-
+        result.extend(get_result_and_create_assets(proj_path))
+        
+        """
+            Support Django style MVC
+            TODO: chocie MVC style
+        """
         os.mkdir(os.path.join(proj_path, "app"))
         app_path = os.path.join(proj_path, "app")
         create_py(os.path.join(app_path, "__init__.py"), 
                 app_init_template.substitute(module=values['module']))
 
-        """
-            Support Django style MVC
-            TODO: chocie MVC style
-        """
-        create_module(app_path, values['module'])
-        
-    except PathAlreadyExists:
+        result.extend(get_result_and_create_module(app_path, values['module']))
+
+        if len(result) > 0:
+            six.print_("\n".join(result))
+
+    except ProjectPathAlreadyExists:
         six.print_("Path Already Exists, use another name!")
+    except InvalidProjectName:
+        six.print_("Invalid Project Name, use another name!")
