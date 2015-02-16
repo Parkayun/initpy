6b8b3e1bc1685adc7c516bd1d7033e022f8a5459
Parkayun <iamparkayun@gmail.com>
2015-02-16 19:08:25 +0900

    bump version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = open(
 
 setup(
     name='flask-init',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/Parkayun/flask-init',
     license='BSD',
     author='Ayun Park',
