c7065b30cce13805c3a5be7c9ddc9896bd4bb86d
Parkayun <iamparkayun@gmail.com>
2015-02-25 19:14:41 +0900

    Add creator test

---
+++ tests/test_creator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+import os
+
+from flask_init.creator import Creator
+
+
+def test_file_create(tmpdir):
+	file_name = 'test'
+
+	creator = Creator(str(tmpdir.realpath()))
+
+	assert not tmpdir.join(file_name).isfile()	
+	creator.create_file(creator.root_path, file_name, '')
+	assert tmpdir.join(file_name).isfile()
