0759193ceaecbdc6fb69d385fadc9966312089be
Parkayun <iamparkayun@gmail.com>
2015-03-04 15:50:34 +0900

    Remove unused exceptions

--- flask_init/exceptions.py
+++ flask_init/exceptions.py
@@ -1,31 +1,3 @@
-class ProjectPathAlreadyExists(Exception):
-    """Base class for Project path already exists"""
-
-
-class StaticPathAlreadyExists(Exception):
-    """Base class for Static path already exists"""
-
-
-class TemplatePathAlreadyExists(Exception):
-    """Base class for Template path already exists"""
-
-
-class ModulePathAlreadyExists(Exception):
-    """Base class for Module path already exists"""
-
-
-class InvalidProjectName(Exception):
-    """Base class for Invalid project name"""
-
-
-class InvalidModuleName(Exception):
-    """Base class for Invalid module name"""
-
-
-class TemplateModuleAlreadyExists(Exception):
-    """Base class for Template Module Already exists"""
-
-
 class InvalidFileName(Exception):
     """Base class for Invalid File name"""
 
